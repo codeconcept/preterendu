9ab102e64fd3bf6d86f7a4afe260ebeb59989e50
Samir Medjdoub <contact@codeconcept.fr>
2017-06-02 21:49:58 +0200

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { StuffComponent } from './stuff/stuff.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StuffComponent
   ],
   imports: [
     BrowserModule,
